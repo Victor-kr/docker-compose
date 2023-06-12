bfa9f9c8fba9d2298d16f22e870565411aded655
root <root@ip-172-17-1-40.ap-northeast-2.compute.internal>
2023-06-12 11:04:11 +0000

    it works

--- get-compose.sh
+++ get-compose.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-export VAULT_ADDR=https://jc.vault.idtplateer.com:8200
-export VAULT_TOKEN=hvs.ZoTCrDt5WjyyWsgkeBj3IjMR
+export VAULT_ADDR=http://vault-lb-34c9b776-478907300.ap-northeast-2.elb.amazonaws.com
+export VAULT_TOKEN=hvs.t3EBl0pkz49Krw7XSSRmSM9Z
 
-cd ./django-sample-for-docker-compose
-vault kv get --format=json kurly/docker/docker-compose | jq -r '.data.data.yml' > ./docker-compose.yml
+vault kv get --format=json config/docker/docker-compose | jq -r '.data.data.yml' > ./docker-compose.yml
 sudo docker-compose up -d
