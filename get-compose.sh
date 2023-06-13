9007272f4c247543c21cea51c6d1866d702bc922
Ubuntu <ubuntu@ip-172-31-51-62.ap-northeast-2.compute.internal>
2023-06-13 04:31:20 +0000

    modify get-compose.sh

--- get-compose.sh
+++ get-compose.sh
@@ -6,6 +6,8 @@
 export VAULT_ADDR=https://jc.vault.idtplateer.com:8200
 export VAULT_TOKEN=$(cat ./plateer-key)
 
+mkdir -p ./docker/data
+
 #vault kv get --format=json config/docker/docker-compose | jq -r '.data.data.yml' > ./docker-compose.yml
 vault kv get --format=json kurly/docker/docker-compose | jq -r '.data.data.yml' > ./docker-compose.yml
 sudo docker-compose up
