f44c044ae93136f96ceafabca78e30ed7080e941
Victor-kr <vicljc@naver.com>
2023-06-13 11:21:51 +0900

    Update get-compose.sh

--- get-compose.sh
+++ get-compose.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #export VAULT_ADDR=http://vault-lb-34c9b776-478907300.ap-northeast-2.elb.amazonaws.com
-#export VAULT_TOKEN=hvs.t3EBl0pkz49Krw7XSSRmSM9Z
+#export VAULT_TOKEN=
 
 export VAULT_ADDR=https://jc.vault.idtplateer.com:8200
-export VAULT_TOKEN=hvs.ZoTCrDt5WjyyWsgkeBj3IjMR
+export VAULT_TOKEN=
 
 #vault kv get --format=json config/docker/docker-compose | jq -r '.data.data.yml' > ./docker-compose.yml
 vault kv get --format=json kurly/docker/docker-compose | jq -r '.data.data.yml' > ./docker-compose.yml
